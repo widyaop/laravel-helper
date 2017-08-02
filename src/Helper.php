0d93136f37be2b559d27f35380f11e4a64ed188e
Pande Putu Widya Oktapratama <widya.oktapratama@gmail.com>
2017-08-03 00:41:50 +0800

    Initialize

---
+++ src/Helper.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: John Doe
+ * Date: 8/3/2017
+ * Time: 12:24 AM
+ */
+
+namespace Helper;
+
+
+class Helper
+{
+
+    public static function upload($url = ''){
+
+    }
+
+}
---
+++ src/Upload.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: John Doe
+ * Date: 8/3/2017
+ * Time: 12:27 AM
+ */
+
+namespace Helper;
+
+use Intervention\Image;
+use Illuminate\Support\Facades\Storage;
+
+
+class Upload
+{
+    public static function image($file , $folder , $name = null , $width = 500 , $height = 500){
+
+    }
+
+}
